acfcdb57abceaadcd823e2397b8cb5d9508b7325
imdylanpro <159092190+imdylanpro@users.noreply.github.com>
2024-02-14 09:43:35 -0800

    Update favorite_languages.py
    Added a portion that shows how to have a dictionary within a list.
    
    This code will also check to see if there is only one item in the dictionary and display a custom message for that scenario.
    
    This code will still display the custom message for the names listed in the friends list.

--- favorite_languages.py
+++ favorite_languages.py
@@ -5,13 +5,13 @@
 # This program will show how to call on the specific key or value
 
 favorite_languages = {
-    'james': 'rust',
-    'dylan': 'Python',
-    'mark': 'c',
-    'sally': 'Assembly',
-    'lane': 'Assembly',
-    'briana': 'haskell'
-}
+    'james': ['rust', 'labview'],
+    'dylan': ['Python', 'assembly'],
+    'mark': ['c',],
+    'sally': ['Assembly', 'go'],
+    'lane': ['Assembly',],
+    'briana': ['haskell', 'dylan'],
+    }
 
 # adds a list that contains a list of friends, so they get a custom message.
 friends =['dylan', 'briana', 'lane', 'james']
@@ -29,11 +29,22 @@ friends =['dylan', 'briana', 'lane', 'james']
 # for n, l in favorite_languages.items():
 #     print(f"One programming language is {l.title()}.")
 
-# Using only the key items in the key-value pair
-for names in favorite_languages.keys():
-    print(f"{names.title()} is a person who casted their vote.")
-    if names in friends:
-        print(f"Hello {names.title()}! Nice to see you :D")
-# Using the value items in the key-value pair
-for languages in favorite_languages.values():
-    print(f"One programming language is {languages.title()}.")
+# # Using only the key items in the key-value pair
+# for names in favorite_languages.keys():
+#     print(f"{names.title()} is a person who casted their vote.")
+#     if names in friends:
+#         print(f"Hello {names.title()}! Nice to see you :D")
+# # Using the value items in the key-value pair
+# for languages in favorite_languages.values():
+#     print(f"One programming language is {languages.title()}.")
+
+for name, languages in favorite_languages.items():
+    print(f"\nHi there, {name.title()}.")
+    if name in friends:
+        print("Good to see you again!")
+    if len(languages) == 1:
+        print(f"Your favorite programming language is: {language}")
+    else:
+        print("Your favorite programming languages are:")
+        for language in languages:
+            print(f"\t{language.title()}")
